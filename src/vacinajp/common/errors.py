3c984bb9e157bd12e3c8ed8c01403e020e09a362
Paulo Freitas <paulogeo5@gmail.com>
2022-08-30 22:00:59 -0300

    feat: add custom domain errors

--- src/vacinajp/common/errors.py
+++ src/vacinajp/common/errors.py
@@ -1,10 +1,22 @@
 class IntegrityError(Exception):
     """Base exception for application integrity errors"""
 
-    ...
+    pass
 
 
 class UserAlreadyExists(IntegrityError):
     """Raised when there is already one user with the same cpf"""
 
-    ...
+    pass
+
+
+class DomainError(Exception):
+    """Base exception for application domain errors"""
+
+    pass
+
+
+class ScheduleNotAvailable(DomainError):
+    """Raised when schedule is not available"""
+
+    pass
